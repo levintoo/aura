208d93a95595509b9f0235649b0bf7f9b1896de6
levintoo <tooklevin@gmail.com>
2024-05-14 22:14:42 +0300

    setup nativewind

---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-3xl">Aura!</Text>
+        <StatusBar style="auto" />
+        <Link href="/profile" style={{ color: 'blue'}}>Go to profile</Link>
+    </View>
+  );
+}
