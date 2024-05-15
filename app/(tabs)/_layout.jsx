8c4e874ee78761655127cd961ec714784d2c1240
levintoo <tooklevin@gmail.com>
2024-05-15 16:01:45 +0300

    design welcome screen

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -12,6 +12,7 @@ const TabIcon = ({ icon, color, name, focused}) => {
       tintColor={color}
       className="w-6 h-6" 
       />
+
       <Text className={`${focused ? 'font-psemibold' : 'font-pregular' } text-xs`} style={{ color: color}}>
         {name}
       </Text>
@@ -40,6 +41,7 @@ const TabsLayout = () => {
           <TabIcon icon={icons.home} color={color} name="Home" focused={focused} />
         )
       }} />
+      
       <Tabs.Screen name="bookmark" options={{
         title: 'Bookmark',
         headerShown: false,
@@ -47,6 +49,7 @@ const TabsLayout = () => {
           <TabIcon icon={icons.bookmark} color={color} name="Bookmark" focused={focused} />
         )
       }} />
+
       <Tabs.Screen name="create" options={{
         title: 'Create',
         headerShown: false,
@@ -54,6 +57,7 @@ const TabsLayout = () => {
           <TabIcon icon={icons.plus} color={color} name="Create" focused={focused} />
         )
       }} />
+
       <Tabs.Screen name="profile" options={{
         title: 'Profile',
         headerShown: false,
--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,61 @@
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import { Link } from "expo-router";
+import { images } from "../constants";
+import PrimaryButton from "../components/PrimaryButton";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { router } from "expo-router";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-        <Text className="text-3xl font-pblack">Aura!</Text>
-        <StatusBar style="auto" />
-        <Link href="/home" style={{ color: 'blue'}}>Go to Home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+
+          <Image
+            source={images.cards}
+            className="max-w--[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless {"\n"} Posibilities with{" "}
+              <Text className="text-secondary-200">Aura</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[70px] h-[15px] absolute -bottom-2 -right-0"
+              resizeMode="contaion"
+            />
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 text-center mt-7">
+            Where innovation meets creativity: embark on a journey {"\n"} of
+            limitless exploration with aura.
+          </Text>
+
+          <PrimaryButton
+            title="Continue with Email"
+            handlePress={() => {
+              router.push("/register");
+            }}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
 }
