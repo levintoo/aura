cf0bc08e6d3db6bb0b238b392919c49ba6db3cd1
levintoo <tooklevin@gmail.com>
2024-05-15 23:25:53 +0300

    implement safeareaview for home

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,18 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text, FlatList } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const home = () => {
   return (
-    <View>
-      <Text>home</Text>
-    </View>
-  )
-}
+    <SafeAreaView>
+      <FlatList
+        data={[{ id: 1 }, { id: 2 }]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => {
+          <Text className="text-3xl">{item.id}</Text>;
+        }}
+      />
+    </SafeAreaView>
+  );
+};
 
-export default home
+export default home;
